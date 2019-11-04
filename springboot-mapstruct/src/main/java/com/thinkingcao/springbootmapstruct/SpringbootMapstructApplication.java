815edbe5e47cfdc6a1fe56ac61bc27becf9a75d7
Thinkingcao <cao1995262118>
2019-11-04 22:07:13 +0800

    新增SpringBoot集成mapstruct模块

---
+++ springboot-mapstruct/src/main/java/com/thinkingcao/springbootmapstruct/SpringbootMapstructApplication.java
@@ -0,0 +1,13 @@
+package com.thinkingcao.springbootmapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMapstructApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMapstructApplication.class, args);
+    }
+
+}
---
+++ springboot-mapstruct/src/test/java/com/thinkingcao/springbootmapstruct/SpringbootMapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkingcao.springbootmapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMapstructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
