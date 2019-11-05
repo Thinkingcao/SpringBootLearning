33dc7356ee85302040af0b8ab108e21df4186eb6
Thinkingcao <617271837@qq.com>
2019-11-05 10:25:09 +0800

    新增SpringBoot整合profile多环境配置

---
+++ springboot-profile/src/main/java/com/thinkingcao/springbootprofile/SpringbootProfileApplication.java
@@ -0,0 +1,13 @@
+package com.thinkingcao.springbootprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProfileApplication.class, args);
+    }
+
+}
