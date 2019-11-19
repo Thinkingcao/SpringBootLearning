c135233875709c4737c27d4844140d77f6c2fda9
Thinkingcao <cao1995262118>
2019-11-19 22:58:26 +0800

    SpringBoot整合Mybatis增删改查XML版

---
+++ springboot-mybatis-xml/src/main/java/com/thinkingcao/springboot/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.thinkingcao.springboot.config;
+
+/**
+ * <pre>
+ * @desc:
+ * @author: cao_wencao
+ * @date: 2019-11-19 22:57
+ * @version: 1.0
+ * </pre>
+ */
+public class SwaggerConfig {
+}
