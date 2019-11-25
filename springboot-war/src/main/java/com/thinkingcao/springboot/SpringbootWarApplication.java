cd276427468e617b6537b3b79715a81ea31885a9
Thinkingcao <617271837@qq.com>
2019-11-25 09:56:53 +0800

    新增SpringBoot整合war包外部部署Tomcat配置

---
+++ springboot-war/src/main/java/com/thinkingcao/springboot/SpringbootWarApplication.java
@@ -0,0 +1,13 @@
+package com.thinkingcao.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWarApplication.class, args);
+    }
+
+}
---
+++ springboot-war/src/test/java/com/thinkingcao/springboot/SpringbootWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkingcao.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
