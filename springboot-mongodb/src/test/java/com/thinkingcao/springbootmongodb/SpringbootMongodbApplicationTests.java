eadfe55d469ef382c965b7af8970b3e061ce42de
Thinkingcao <617271837@qq.com>
2019-12-05 17:37:34 +0800

    SpringBoot整合mongodb

---
+++ springboot-mongodb/src/test/java/com/thinkingcao/springbootmongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkingcao.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
