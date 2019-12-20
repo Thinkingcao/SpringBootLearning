ce0db3c483d14f0d39d538a6e05e6c4390267f47
Thinkingcao <617271837@qq.com>
2019-12-20 17:16:34 +0800

    Spring Boot使用注解控制Api接口幂等性之前后端分离架构设计

--- springboot-aop-annotation/src/main/java/com/thinkingcao/springboot/aop/SpringbootAopApplication.java
+++ springboot-aop-annotation/src/main/java/com/thinkingcao/springboot/aop/SpringbootAopApplication.java
@@ -4,6 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @desc: 启动类； 注意: 注解式mybatis的sql需要加上@MapperScan扫描，xml形式的mybatis的sql不需要
+ * @auth: cao_wencao
+ * @date: 2019/12/20 17:14
+ */
 @SpringBootApplication
 @MapperScan(value = "com.thinkingcao.springboot.aop.mapper")
 public class SpringbootAopApplication {
