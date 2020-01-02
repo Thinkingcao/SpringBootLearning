470c91c8669f8adfdab0ac36e8e1bc480ef41398
Thinkingcao <cao1995262118>
2020-01-02 23:42:46 +0800

    Spring Boot整合MongoDB

--- springboot-mongodb/src/main/java/com/thinkingcao/springbootmongodb/controller/OrderController.java
+++ springboot-mongodb/src/main/java/com/thinkingcao/springbootmongodb/controller/OrderController.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
 import java.util.List;
 
 /**
