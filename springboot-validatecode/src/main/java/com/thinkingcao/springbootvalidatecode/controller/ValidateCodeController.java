57dfd3e00933b016612612a9707fd95234d563ca
Thinkingcao <617271837@qq.com>
2020-01-03 15:30:44 +0800

    SpringBoot+Redis生成图片验证码并校验

--- springboot-validatecode/src/main/java/com/thinkingcao/springbootvalidatecode/controller/ValidateCodeController.java
+++ springboot-validatecode/src/main/java/com/thinkingcao/springbootvalidatecode/controller/ValidateCodeController.java
@@ -1,6 +1,7 @@
 package com.thinkingcao.springbootvalidatecode.controller;
 
 import com.google.common.collect.Maps;
+import com.thinkingcao.springbootvalidatecode.result.ResponseCode;
 import com.thinkingcao.springbootvalidatecode.utils.ImgValidateCodeUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,48 +21,56 @@ import java.util.concurrent.TimeUnit;
  */
 @Slf4j
 @RestController
-@RequestMapping("/code")
+@RequestMapping("/api")
 public class ValidateCodeController {
 
     @Autowired
     private StringRedisTemplate redisTemplate;
 
     /**
-     * 生成图片验证码
-     * @return
+     * @url： http://127.0.0.1:8080/api/getImgCode
+     * @desc: 生成图片验证码
+     * @auth: cao_wencao
+     * @date: 2020/1/3 14:43
      */
     @GetMapping("/getImgCode")
-    public Map<String, String> getImgCode() {
+    public ResponseCode getImgCode() {
         //初始化一个map
         Map<String, String> result = Maps.newHashMap();
         try {
             // 获取 4位数验证码
             result= ImgValidateCodeUtil.getImgCodeBaseCode(4);
+            log.info("生成图片验证码 : {}", result);
             // 将验证码存入redis 中（有效时长5分钟）
             setImgCodeCache(result);
         } catch (Exception e) {
             log.info(e.getMessage());
         }
-        return result;
+        return ResponseCode.success("生产图片验证码成功", result);
     }
 
+
     /**
-     * 校验验证码
+     * @url:  http://127.0.0.1:8080/api/getImgCode
+     * @desc: 校验验证码
      * @param imgCodeKey
      * @param imgCode
-     * @return
+     * @auth: cao_wencao
+     * @date: 2020/1/3 14:43
      */
     @GetMapping("/checkImgCode")
-    public String checkImgCode(String imgCodeKey, String imgCode) {
+    public ResponseCode checkImgCode(String imgCodeKey, String imgCode) {
         String cacheCode = redisTemplate.opsForValue().get(imgCodeKey);
         if (null == cacheCode) {
-            return "图片验证码已过期，请重新获取";
+            return ResponseCode.error("图片验证码已过期，请重新获取");
         }
         if (cacheCode.equals(imgCode.toLowerCase())) {
-            return "验证码输入正确";
+            log.debug("cacheCode: {} ,imgCode : {}", cacheCode , imgCode);
+            return ResponseCode.success("验证码校验正确",cacheCode);
         }
-        return "验证码输入错误";
+        return ResponseCode.error("验证码输入错误");
     }
+
 
     /**
      * 将验证码存入redis 中
