e8d937918b568b44452096c6891089d912f20fd5
Thinkingcao <617271837@qq.com>
2020-01-03 12:50:27 +0800

    Spring Boot整合RocketMQ

--- spring-boot-rocketmq/spring-boot-rocketmq-producer/src/main/java/com/thinkingcao/rocketmq/SpringBootRocketmqProducerApplication.java
+++ spring-boot-rocketmq/spring-boot-rocketmq-producer/src/main/java/com/thinkingcao/rocketmq/SpringBootRocketmqProducerApplication.java
@@ -1,13 +1,8 @@
 package com.thinkingcao.rocketmq;
 
-import com.thinkingcao.rocketmq.entity.OrderPaidEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.Resource;
 /**
  * <pre>
  * @desc: 生产者启动类
