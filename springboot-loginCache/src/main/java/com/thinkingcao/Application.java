59a1f7163e464f1f6722ebb2bd0e6ee72a2da68f
Thinkingcao <617271837@qq.com>
2020-07-16 17:45:30 +0800

    SpringBoot登录、登出

---
+++ springboot-loginCache/src/main/java/com/thinkingcao/Application.java
@@ -0,0 +1,16 @@
+package com.thinkingcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @desc:  启动类
+ * @author: cao_wencao
+ * @date: 2020-07-16 17:03
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ springboot-loginCache/src/main/java/com/thinkingcao/controller/LogoutController.java
@@ -0,0 +1,9 @@
+package com.thinkingcao.controller;
+
+/**
+ * @desc:
+ * @author: cao_wencao
+ * @date: 2020-07-16 17:19
+ */
+public class LogoutController {
+}
