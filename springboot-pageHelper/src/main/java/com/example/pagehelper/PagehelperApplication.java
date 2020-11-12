c6257a624093fd96a1eb94b14936c834f8c083ee
Thinkingcao <617271837@qq.com>
2020-11-12 16:52:25 +0800

    SpringBoot整合PageHelper分页插件实现分页

--- springboot-pageHelper/src/main/java/com/example/pagehelper/PagehelperApplication.java
+++ springboot-pageHelper/src/main/java/com/example/pagehelper/PagehelperApplication.java
@@ -1,9 +1,14 @@
 package com.example.pagehelper;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication
+@MapperScan(basePackages = {"com.example.pagehelper.**.mapper"})//扫描mybatis的所有**Mapper接口文件
 public class PagehelperApplication {
 
     public static void main(String[] args) {
---
+++ springboot-pageHelper/src/main/java/com/example/pagehelper/service/OrderService.java
@@ -0,0 +1,61 @@
+package com.example.pagehelper.service;
+
+import com.example.pagehelper.entity.Order;
+import java.util.List;
+
+/**
+ * (TOrder)表服务接口
+ *
+ * @author makejava
+ * @since 2020-11-12 14:30:23
+ */
+public interface OrderService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param oid 主键
+     * @return 实例对象
+     */
+    Order queryById(Integer oid);
+
+    /**
+     * 查询多条数据
+     *
+     * @param offset 查询起始位置
+     * @param limit 查询条数
+     * @return 对象列表
+     */
+    List<Order> queryAllByLimit(int offset, int limit);
+
+    /**
+     * 查询所有数据
+     * @return
+     */
+    List<Order> selectAll();
+
+    /**
+     * 新增数据
+     *
+     * @param order 实例对象
+     * @return 实例对象
+     */
+    Order insert(Order order);
+
+    /**
+     * 修改数据
+     *
+     * @param order 实例对象
+     * @return 实例对象
+     */
+    Order update(Order order);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param oid 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Integer oid);
+
+}
