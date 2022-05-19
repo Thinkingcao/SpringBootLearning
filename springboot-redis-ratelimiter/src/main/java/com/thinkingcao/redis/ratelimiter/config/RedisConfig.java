f6edda6cbfab1c50e844fdee366af6b752941f5c
Thinkingcao <Thinkingcao@163.com>
2022-05-19 19:05:44 +0800

    SpringBoot整合Redis+AOP+自定义注解实现API接口限流

--- springboot-redis-ratelimiter/src/main/java/com/thinkingcao/redis/ratelimiter/config/RedisConfig.java
+++ springboot-redis-ratelimiter/src/main/java/com/thinkingcao/redis/ratelimiter/config/RedisConfig.java
@@ -12,6 +12,12 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    /**
+     * 默认使用的RedisTemplate底层序列化时采用的是JDK自带的序列化,在getKey的时候Redis中的key值会莫名多一些二进制前缀,取值会有问题
+     * 这里使用Jackson来序列化,也可以选择fastJSON
+     * @param connectionFactory
+     * @return
+     */
     @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
@@ -19,9 +25,12 @@ public class RedisConfig {
         // 创建Jackson2JsonRedisSerialize序列化器,使用Jackson2JsonRedisSerialize 替换默认序列化(默认采用的是JDK序列化)
         Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         ObjectMapper om = new ObjectMapper();
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
+
         redisTemplate.setKeySerializer(jackson2JsonRedisSerializer);
         redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
         redisTemplate.setHashKeySerializer(jackson2JsonRedisSerializer);
